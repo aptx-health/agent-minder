68769e018bd52eebadfd7cb455dc77f0ef1a0d5d
Test <test@test.com>
2026-03-19 13:16:17 -0600

    Add goreleaser config and CI release workflow - Fixes #206

--- main.go
+++ main.go
@@ -2,6 +2,14 @@ package main
 
 import "github.com/dustinlange/agent-minder/cmd"
 
+// Version info set by goreleaser via -ldflags.
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+)
+
 func main() {
+	cmd.SetVersionInfo(version, commit, date)
 	cmd.Execute()
 }
